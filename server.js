a46253ee21e39aa5ab530bba4322fbe7409b8d39
Milan Kranti <milan.kranti@FVFJ3MWWQ6LR.local>
2025-06-22 19:47:04 +0530

    ff

--- server.js
+++ server.js
@@ -6,12 +6,14 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '/')));
+
+// Serve static files - updated for Vercel
+app.use(express.static(__dirname));
 
 // Database setup
 const db = new sqlite3.Database('./users.db', (err) => {
